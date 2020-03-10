8ec468a4f2de587cfd308881c0c3871467f16979
Marcelo Damasceno de Melo <marcelo.damasceno@ifrn.edu.br>
2020-03-10 11:13:40 -0300

    Adiciona metodos que tratam a requisicao com variaveis de caminho e com parametros de caminho

--- src/main/java/com/marcelodamasceno/PaginaInicial.java
+++ src/main/java/com/marcelodamasceno/PaginaInicial.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.marcelodamasceno.model.Cliente;
@@ -19,11 +20,25 @@ public class PaginaInicial {
 		return "<h1>Olá Marcelo</h1>";
 	}
 	
+//	@GetMapping("/cliente/{primeiroNome}")
+//	public ModelAndView olaCliente(@PathVariable("primeiroNome") String primeiroNome, Model model) {
+//		cliente.setPrimeiroNome(primeiroNome);
+//		model.addAttribute("cliente",cliente);
+//		return new ModelAndView("ola_usuario","cliente",cliente);
+//	}
+	
 	@GetMapping("/cliente/{primeiroNome}")
-	public ModelAndView olaCliente(@PathVariable("primeiroNome") String primeiroNome, Model model) {
+	public String olaClientePathVariable(@PathVariable("primeiroNome") String primeiroNome, Model model) {
 		cliente.setPrimeiroNome(primeiroNome);
 		model.addAttribute("cliente",cliente);
-		return new ModelAndView("ola_usuario","cliente",cliente);
+		return "ola_usuario";
+	}
+	
+	@GetMapping("/cliente")
+	public String olaClienteRequestParam(@RequestParam("primeiroNome") String primeiroNome, Model model) {
+		cliente.setPrimeiroNome(primeiroNome);
+		model.addAttribute("cliente",cliente);
+		return "ola_usuario";
 	}
 
 }
