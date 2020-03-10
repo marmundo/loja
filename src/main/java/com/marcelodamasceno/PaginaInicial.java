d38c48c3273517a2afb267d225dab71c51af7770
Marcelo Damasceno de Melo <marcelo.damasceno@ifrn.edu.br>
2020-03-10 10:39:27 -0300

    Adiciona pagina que consegue captar nome do usuario pela url e exibir em uma página em branco

--- src/main/java/com/marcelodamasceno/PaginaInicial.java
+++ src/main/java/com/marcelodamasceno/PaginaInicial.java
@@ -1,14 +1,29 @@
 package com.marcelodamasceno;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import com.marcelodamasceno.model.Cliente;
+
+@Controller
 public class PaginaInicial {
+	
+
+	Cliente cliente=new Cliente();
 	
 	@GetMapping("/")
 	public String home() {
 		return "<h1>Olá Marcelo</h1>";
+	}
+	
+	@GetMapping("/cliente/{primeiroNome}")
+	public ModelAndView olaCliente(@PathVariable("primeiroNome") String primeiroNome, Model model) {
+		cliente.setPrimeiroNome(primeiroNome);
+		model.addAttribute("cliente",cliente);
+		return new ModelAndView("ola_usuario","cliente",cliente);
 	}
 
 }
