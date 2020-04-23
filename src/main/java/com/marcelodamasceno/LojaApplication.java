626f42ff021e9fb4f061557a3555627804984948
Marcelo Damasceno de Melo <marcelo.damasceno@ifrn.edu.br>
2020-04-23 20:20:20 -0300

    adiciona thymeleaf dialect

--- src/main/java/com/marcelodamasceno/LojaApplication.java
+++ src/main/java/com/marcelodamasceno/LojaApplication.java
@@ -2,9 +2,17 @@ package com.marcelodamasceno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @SpringBootApplication
 public class LojaApplication {
+	
+	@Bean
+	public LayoutDialect layoutDialect() {
+	    return new LayoutDialect();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
